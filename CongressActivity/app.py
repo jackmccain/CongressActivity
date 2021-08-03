1ef87955d52cb95113347d63b5b0c3ddc3e9d9ed
Jack McCain <jackmccain56@gmail.com>
2021-08-03 14:46:50 -0400

    Drafted dashboard

--- CongressActivity/app.py
+++ CongressActivity/app.py
@@ -10,27 +10,36 @@ app = Flask(__name__)
 PROPUBLICA_API_KEY = os.getenv("PROPUBLICA_API_KEY")
 
 CONGRESS_DATA = requests.get(
-    "https://api.propublica.org/congress/v1/116/senate/members.json",
+    "https://api.propublica.org/congress/v1/117/house/members.json",
     headers={"X-API-Key": PROPUBLICA_API_KEY},
 ).json()
-response = CONGRESS_DATA["results"][0]
+
+SENATE_DATA = requests.get(
+    "https://api.propublica.org/congress/v1/117/senate/members.json",
+    headers={"X-API-Key": PROPUBLICA_API_KEY},
+).json()
+
+
+
+
+legislators = CONGRESS_DATA["results"][0]["members"] + SENATE_DATA["results"][0]["members"]
 
 
 @app.route("/")
 def home():
     return render_template(
-        "pages/dash.html",
+        "pages/home.html",
     )
 
 @app.route("/official")
 def official():
     officials = request.args.getlist("official")
 
-    matched_officials = filter(lambda k: (k["first_name"] + " " + k["last_name"] in officials), response["members"])
+    matched_officials = filter(lambda k: (k["first_name"] + " " + k["last_name"] in officials), legislators)
 
     return render_template(
         "pages/official.html",
          officials=matched_officials
     )
-    
+
 app.run(debug=True)
